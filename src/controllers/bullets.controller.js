e6841f2eb337d760b80072d4067097de4a2f48d5
Aarun1349 <arunmahendra90@gmail.com>
2024-04-12 22:04:35 +0530

    complete bullet controller

--- src/controllers/bullets.controller.js
+++ src/controllers/bullets.controller.js
@@ -33,4 +33,152 @@ const addNewBullet = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse(200, newBullet, "success"));
 });
 
-export { addNewBullet };
+const getTodaysBulletOfUser = asyncHandler(async (req, res) => {
+  const userId = req.user?.id;
+  if (!userId) {
+    throw new ApiError(404, "Please login first");
+  }
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const todaysBullet = await Bullet.find({
+    user: userId,
+    dateField: { $gte: today, $lt: new Date(today.getTime() + 86400000) },
+  });
+  console.log(todaysBullet.length);
+  if (todaysBullet.length <= 0) {
+    throw new ApiError(404, "No tasks found for today, Enjoy your 🍵");
+  }
+  res.status(200).json(new ApiResponse(200, todaysBullet, "success"));
+});
+
+const changeState = asyncHandler(async (req, res) => {
+  const userId = req.user?.id;
+  const { state } = req.body;
+  const { bulletId } = req.params;
+  if (!userId) {
+    throw new ApiError(404, "Please login first");
+  }
+  const updateBullet = await Bullet.findByIdAndUpdate({
+    _id: bulletId,
+    user: userId,
+  });
+  if (!updateBullet) {
+    throw new ApiError(404, "Requested bullet is not found");
+  }
+  updateBullet.state = state;
+  await updateBullet.save({ validateBeforeSave: false }, { new: true });
+  res.status(200).json(new ApiResponse(200, updateBullet, "success"));
+});
+
+const markDone = asyncHandler(async (req, res) => {
+  const userId = req.user?.id;
+  const { bulletId } = req.params;
+  if (!userId) {
+    throw new ApiError(404, "Please login first");
+  }
+  const markComplete = await Bullet.findByIdAndUpdate({
+    _id: bulletId,
+    user: userId,
+  });
+  if (!markComplete) {
+    throw new ApiError(404, "Requested bullet is not found");
+  }
+
+  markComplete.state = "Completed";
+  await markComplete.save({ validateBeforeSave: false }, { new: true });
+  res.status(200).json(new ApiResponse(200, markComplete, "success"));
+});
+
+const getBullet = asyncHandler(async (req, res) => {
+  const userId = req.user?.id;
+
+  const { bulletId } = req.params;
+  if (!userId) {
+    throw new ApiError(404, "Please login first");
+  }
+  const bullet = await Bullet.findById({
+    _id: bulletId,
+    user: userId,
+  });
+  if (!bullet) {
+    throw new ApiError(404, "Requested bullet is not found");
+  }
+  res.status(200).json(new ApiResponse(200, bullet, "success"));
+});
+
+const deleteBullet = asyncHandler(async (req, res) => {
+  const userId = req.user?.id;
+
+  const { bulletId } = req.params;
+  if (!userId) {
+    throw new ApiError(404, "Please login first");
+  }
+  const bullet = await Bullet.findById({
+    _id: bulletId,
+    user: userId,
+  });
+
+  if (!bullet) {
+    throw new ApiError(404, "Requested bullet is not found");
+  }
+   await Bullet.findByIdAndDelete({ _id: bulletId, user: userId });
+  res.status(200).json(new ApiResponse(200, "success"));
+});
+
+const changeDate = asyncHandler(async (req, res) => {
+  const userId = req.user?.id;
+  const { dateField } = req.body;
+  const { bulletId } = req.params;
+  if (!userId) {
+    throw new ApiError(404, "Please login first");
+  }
+  const updateBulletDate = await Bullet.findByIdAndUpdate({
+    _id: bulletId,
+    user: userId,
+  });
+  if (!updateBulletDate) {
+    throw new ApiError(404, "Requested bullet is not found");
+  }
+  updateBulletDate.dateField=dateField
+  updateBulletDate.state="ReScheduled"
+  await updateBulletDate.save({validateBeforeSave:false,new:true})
+  res.status(200).json(new ApiResponse(200, updateBulletDate, "success"));
+});
+
+const updateBullet = asyncHandler(async (req, res) => {
+  const userId = req.user?.id;
+  const { bullet, priority, alligned_to } = req.body;
+  const { bulletId } = req.params;
+  if (!userId) {
+    throw new ApiError(404, "Please login first");
+  }
+  const updateBullet = await Bullet.findByIdAndUpdate({
+    _id: bulletId,
+    user: userId,
+    bullet,
+    priority,
+    alligned_to,
+  });
+  if (!updateBullet) {
+    throw new ApiError(404, "Requested bullet is not found");
+  }
+  updateBullet.bullet = bullet;
+  priority ? (updateBullet.priority = priority) : updateBullet.priority;
+  alligned_to
+    ? (alligned_to.alligned_to = alligned_to)
+    : updateBullet.alligned_to;
+  updateBullet.bullet = bullet;
+  await updateBullet.save({ validateBeforeSave: false }, { new: true });
+  res.status(200).json(new ApiResponse(200, updateBullet, "success"));
+});
+
+export {
+  addNewBullet,
+  getTodaysBulletOfUser,
+  changeState,
+  getBullet,
+  changeDate,
+  deleteBullet,
+  markDone,
+  updateBullet,
+};
--- src/routes/bullet.route.js
+++ src/routes/bullet.route.js
@@ -1,10 +1,28 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { addNewBullet } from "../controllers/bullets.controller.js";
+import {
+  addNewBullet,
+  getTodaysBulletOfUser,
+  changeState,
+  changeDate,
+  deleteBullet,
+  getBullet,
+  updateBullet,
+  markDone,
+} from "../controllers/bullets.controller.js";
 
 const bulletRouter = Router();
 
 bulletRouter.use(verifyJWT);
 
 bulletRouter.route("/newBullet").post(addNewBullet);
+bulletRouter.route("/today").get(getTodaysBulletOfUser);
+bulletRouter.route("/change-state/:bulletId").put(changeState);
+bulletRouter.route("/change-date/:bulletId").put(changeDate);
+bulletRouter
+  .route("/:bulletId")
+  .delete(deleteBullet)
+  .get(getBullet)
+  .put(updateBullet)
+  .patch(markDone);
 export default bulletRouter;
