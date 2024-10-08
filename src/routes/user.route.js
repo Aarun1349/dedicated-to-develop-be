0193bcdb29b84b688993b5b9f9ff7a069ecc61c0
aruns <aruns@LAPTOP-NMBB3SHL>
2024-10-08 17:13:25 +0530

    user route updated

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,10 @@ import {
   userSignUp,
   userSignIn,
   logoutUser,
+  changeCurrentPassword,
+  updateUserDetails,
+  refreshAccessToken,
+  getCurrentUser
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -10,6 +14,10 @@ const userRouter = Router();
 
 userRouter.route("/login").post(userSignIn);
 userRouter.route("/logout").post(verifyJWT, logoutUser);
+userRouter.route("/:user_id").get(verifyJWT, getCurrentUser);
+userRouter.route("/update/:user_id").patch(verifyJWT, updateUserDetails);
+userRouter.route("/changePassword/:user_id").patch(verifyJWT, changeCurrentPassword);
+userRouter.route("/refresh/:user_id").patch (verifyJWT, refreshAccessToken);
 userRouter.route("/signup").post(
   upload.fields([
     { name: "avatar", maxCount: 1 },
